75cb00322a75435cb9aba3a631b36580caf8f05d
Sayeeshwar <sayeeshwar.girish@gmail.com>
2019-06-23 14:13:58 +0530

    Add proxy to API to avoid cross-origin error

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ window.addEventListener("load",()=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api=`https://api.darksky.net/forecast/c3bc6195d4ef71460f4b10c65cd8ca14/37.8267,-122.4233`;
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api=`${proxy}https://api.darksky.net/forecast/c3bc6195d4ef71460f4b10c65cd8ca14/37.8267,-122.4233`;
 
         })
     }
